a40fdc5b0f47f0e3f21b838b632bd346235eb033
Luis Rivera <luis@riverside88.com>
2026-09-25 13:04:28 -0400

    Follow catalog default branch

--- src/catalog-config.js
+++ src/catalog-config.js
@@ -1,1 +1,1 @@
-export const CATALOG_MANIFEST_URL = "https://raw.githubusercontent.com/dev-rix/cloud-gaming-finder-catalog/7c19fc647593b1c060adb0a14f09e5ea596621c7/catalog/manifest.json";
+export const CATALOG_MANIFEST_URL = "https://raw.githubusercontent.com/dev-rix/cloud-gaming-finder-catalog/master/catalog/manifest.json";
